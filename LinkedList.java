34ff7ed0dd24d8ee78457b46650d4952c40d4175
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2020-10-15 20:11:29 -0700

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,30 @@
+public class LinkedList
+{
+  // 6 Functions
+  public LinkedList()
+  {
+    //add code here
+  }
+  public void add(E item)
+  {
+    //add code here
+  }
+  public void add(E item, int position)
+  {
+    //add code here
+  }
+  public E get(int position)
+  {
+    //add code here
+  }
+  public E remove(int position)
+  {
+    //add code here
+  }
+  public E reverse(E head)
+  {
+    //add code here
+  }
+  
+  // Add driver program
+}
